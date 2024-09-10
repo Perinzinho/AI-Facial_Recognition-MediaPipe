16fff7af982cbd39f4783ffd1c06534b98206f0d
Noah Veri Franco <noahvf16@outlook.com>
2024-09-10 09:25:14 -0300

    biblioteca install

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
 #OBS:Precisa ser alterado o codigo para o mediapipe, editar também o readme
+#para utilizar o mediapipe com o codigo é necessario que voce instale o git no pc
+#abrir e colocar o codigo: pip install mediapipe
+import mediapipe as mp
+
