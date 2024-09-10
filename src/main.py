83ced0994b1a78f7189f800ccbac14ed5a89ed5b
Leonardo Perin Andreozi <155500125+Perinzinho@users.noreply.github.com>
2024-09-10 13:53:43 -0300

    Fazendo a camera dar vídeo e arrumando os erros de Sintaxe

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #OBS:Precisa ser alterado o codigo para o mediapipe, editar também o readme
 #para utilizar o mediapipe com o codigo é necessario que voce instale o git no pc
 #abrir e colocar o codigo: pip install mediapipe opencv-python
+
+#Import da Biblioteca da Webcam
 import mediapipe as mp
 
 import cv2
