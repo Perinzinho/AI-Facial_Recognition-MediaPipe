54ec1e22bd929d9099142487ed25af2d27aca299
Noah Veri Franco <noahvf16@outlook.com>
2024-09-10 11:08:55 -0300

    desenho dos contornos dos olhos, boco, etc.

--- src/main.py
+++ src/main.py
@@ -44,3 +44,14 @@ with mp_face_mesh.Facemash(
                     landmark_drawing_spec=mp_drawing_styles
                     .get_default_face_mesh_tesselation_style()
                 )
+
+                #desenho dos contornos dos olhos, boco, etc.
+                mp_drawing.draw_landmarks(
+                    image=image,
+                    landmark_list=face_landmarks,
+                    connections=mp_face_mesh.FACEMESH_CONTOURS,
+                    landmark_drawing_spec=None,
+                    connection_drawinf_spec=mp_drawing_styles
+                    .get_default_face_mesh_contours_style()
+                )
+
