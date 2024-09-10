eb277d786a7604d1e69b10bdc3c4a858e1d636d5
Noah Veri Franco <noahvf16@outlook.com>
2024-09-10 09:53:25 -0300

    Inicialização do facemesh

--- src/main.py
+++ src/main.py
@@ -14,3 +14,15 @@ mp_drawing_styles = mp.solutions.drawing_styles
 #configuração da webcam
 cap = cv2.VideoCapture(0)
 
+#Inicializa o facemesh
+with mp_face_mesh.Facemash(
+    max_num_faces=1, #detecta até 1 rosto(adicionar + se precisar)
+    refine_landmarks=True, #Refinamento p/ olhos e labios
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as face_mesh:
+
+    while cap.isOpened():
+        sucess, image = cap.read()
+        if not sucess:
+            print("Não foi possivel capturar a imagem")
+            break
